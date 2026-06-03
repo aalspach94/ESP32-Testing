cea8eaad572a4a6b55fddc72aa211f654ac82519
Andrew Alspach <andrewalspach94@gmail.com>
2026-06-02 23:59:10 -0400

    add init-setup

---
+++ code lib/init-setup.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+//dont remove!
+
+// #define or int variables here
+
+
+//setup code, runs once at startup
+void setup() {
+
+}
+
+
+//loop code, runs repeatedly after setup
+void loop() {
+
+}
